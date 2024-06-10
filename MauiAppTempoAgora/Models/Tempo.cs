b02fd5c48a98fee3be4a2b745f1f85a7be0f0633
Tiago <frost.tiago@gmail.com>
2024-06-10 13:34:47 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/Models/Tempo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppTempoAgora.Models
+{
+    public class Tempo
+    {
+        public string? Title { get; set; }
+        public string? Temperature { get; set; }
+        public string? Wind { get; set; }
+        public string? Humidity { get; set; }
+        public string? Visibility { get; set; }
+        public string? Sunrise { get; set; }
+        public string? Sunset { get; set; }
+
+    }
+}
---
+++ MauiAppTempoAgora/Service/DataService.cs
@@ -0,0 +1,31 @@
+﻿using MauiAppTempoAgora.Models;
+using System.Text.Json;
+
+namespace MauiAppTempoAgora.Service
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "3a1c2788d3f9ad95494df4258594b553";
+
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&appid={appId}";
+
+            Tempo? tempo = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    tempo = JsonSerializer.Deserialize<Tempo>(json);                    
+                }
+            }
+
+            return tempo;
+        }
+    }
+}
