a93d99b4b979dff534171cdf3ad6e4d4719ceb7e
Tiago <tiago@tiago.blog.br>
2024-06-11 22:15:23 -0300

    Add geolocalização

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -9,16 +9,49 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
 
+            string teste = await GetCachedLocation();
+
             if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+                CounterBtn.Text = $"Clicked {teste} time";
             else
-                CounterBtn.Text = $"Clicked {count} times";
+                CounterBtn.Text = $"Clicked {teste} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public async Task<string> GetCachedLocation()
+        {
+            try
+            {
+                Location location = await Geolocation.Default.GetLocationAsync();
+
+                Console.WriteLine(location);
+
+                if (location != null)
+                    return $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+
+            return "None";
         }
     }
 
